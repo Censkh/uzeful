c637356c5a4c1aeb08856720056843af5ff50486
James Waterhouse <09jwater@gmail.com>
2025-02-18 21:38:33 +0700

    WIP: add WIP cli + openAPI entry

---
+++ cli/index.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { program } from "commander";
+import { Project, PropertyAccessExpression, SyntaxKind } from "ts-morph";
+
+program.name("uze");
+
+const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json"), "utf-8"));
+program.version(packageJson.version);
+
+program
+  .command("generate")
+  .argument("<entry>", "Path to application entry")
+  .description("Generate OpenAPI spec from a uze application")
+  .action(async (entry, options) => {
+    const project = new Project();
+    const sourceFile = project.addSourceFileAtPath(entry);
+    const uzeImport = sourceFile.getImportDeclaration("uze");
+    if (!uzeImport) {
+      console.error("uze import not found");
+      process.exit(1);
+    }
+
+    for (const callExpression of sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression)) {
+      const entryExpression = callExpression
+        .getChildrenOfKind(SyntaxKind.Identifier)
+        .find((id) => id.getText() === "openApiEntry");
+      if (entryExpression) {
+        console.log(entryExpression.getDefinitions());
+      }
+    }
+  });
+
+program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import type { Router } from "./router";
+
 export type { Middleware, Route, Uze, UzeAdapter } from "./Types";
 export { SendableError, ErrorCode } from "sendable-error";
 export { uzeState, type StateKey, createStateKey } from "./State";
@@ -9,3 +11,7 @@ export * from "./BuiltinHooks";
 export { type Context, createUzeContextHook, runWithContext } from "./Context";
 
 export { createUze } from "./CreateUze";
+
+export function openApiEntry(router: Router) {
+  // used for static analysis
+}
